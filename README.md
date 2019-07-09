d1e8411d6227fcadfc89f4c3d25c6be67ef476fc
Piss <Piss>
2019-07-09 08:36:08 -0500

    Much more elegant. Recursively walks through and inserts nodes from given html. Creates a script tag from script tags (By default, elem.innerHTML won't run scripts - they're basically inserted as text elements), else it just places them on the page.

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# load-script-async
+<a name="loadScriptAsync"></a>
+
+## loadScriptAsync
+Allows loading html, including scripts - similarto jQuery's ".html()"
+
+**Kind**: global class  
+**Author:**: footjohnson  
+
+* [loadScriptAsync](#loadScriptAsync)
+    * [.ReplaceHtml(html, domLocation)](#loadScriptAsync+ReplaceHtml) ⇒ <code>boolean</code>
+    * [.CheckForScripts(node)](#loadScriptAsync+CheckForScripts) ⇒ <code>boolean</code>
+    * [.PlaceElems(elems, domLocation)](#loadScriptAsync+PlaceElems) ⇒ <code>boolean</code>
+
+<a name="loadScriptAsync+ReplaceHtml"></a>
+
+### loadScriptAsync.ReplaceHtml(html, domLocation) ⇒ <code>boolean</code>
+Replaces html on page with given html, executing scripts in the process.
+
+**Kind**: instance method of [<code>loadScriptAsync</code>](#loadScriptAsync)  
+**Returns**: <code>boolean</code> - True if success; false if failure  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| html | <code>string</code> | Html to replace current html |
+| domLocation | <code>HTMLElement</code> | Node whose html should be replaced |
+
+<a name="loadScriptAsync+CheckForScripts"></a>
+
+### loadScriptAsync.CheckForScripts(node) ⇒ <code>boolean</code>
+Checks if there are any script tags in the node
+
+**Kind**: instance method of [<code>loadScriptAsync</code>](#loadScriptAsync)  
+**Returns**: <code>boolean</code> - True if it contains scripts; false if it doesn't  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| node | <code>Node</code> | Node to check for scripts |
+
+<a name="loadScriptAsync+PlaceElems"></a>
+
+### loadScriptAsync.PlaceElems(elems, domLocation) ⇒ <code>boolean</code>
+Recursively places html elements on the DOM
+
+**Kind**: instance method of [<code>loadScriptAsync</code>](#loadScriptAsync)  
+**Returns**: <code>boolean</code> - True if success; false if failure  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| elems | <code>HTMLCollection</code> | Elements to place on DOM |
+| domLocation | <code>Node</code> | Where to put the elements |
+
--- loadScriptAsync.js
+++ loadScriptAsync.js
@@ -1,158 +1,169 @@
-/**
-    * Replaces html on page with given html, executing scripts in the process.
-    * @author: footjohnson
-    * 
-    * @param {string} html Html to replace current html
-    * @param {HTMLElement} domLocation Node whose html should be replaced
-    * 
-    * @returns {boolean} True if success; false if failure
-*/
-function ReplaceHtml(html, domLocation){
-    try{
-        const dp = new DOMParser();
-        const doc = dp.parseFromString(html.toString(), "text/html");
-        domLocation.innerHTML = html;
-        
-        scripts = doc.getElementsByTagName("script");
-
-        for(i in scripts){
-            const script = scripts[i];
-            domLocation.appendChild(script);
-        };
-
-        const scriptSets = ExtractScripts(html);
-        ExecuteScripts(scriptSets, domLocation);
-    }
-    catch(e){
-        console.log(e);
-        return false;
-    }
-    return true;
-}
+"strict mode"
 
 /**
-    * Executes scripts returned from asynchronous html responses 
-    * (so I never have to use jQuery again) and places html on DOM
-    * @author: footjohnson
-    * 
-    * @param {Array<HTMLElement>} rawScripts Scripts to execute (via DOM placement)
-    * @param {HTMLElement} domLocation Where to place scripts on page
-    * 
-    * @returns {boolean} returns true if success or false if failure 
-    * 
-    * @TODO Actually link this shit up
-*/
-function ExecuteScripts(scripts, domLocation){
-    try{
-        for(i in scripts){
-            const scriptCouple = scripts[i];
-            const script = scriptCouple[0];
-            const isSrc = scriptCouple[1];
-            const s = document.createElement("script");
-            if(isSrc){
-                s.type = "text/javascript";
-                s.src = script;
+ * Allows loading html, including scripts - similar
+ * to jQuery's ".html()"
+ * 
+ * @author: footjohnson
+ */
+class loadScriptAsync{
+    constructor(){
+        this.CreateLSARegex();
+    }
+
+    // Creates necessary regular expressions
+    CreateLSARegex(){
+        class LSA_REGEX_Class{
+            constructor(){
+                this.SetFields();
+                this.CreateSrcRegEx();
             }
-            else{
-                const t = document.createTextNode(script);
-                console.log(script);
-                
-                s.appendChild(t);
-            }
-            domLocation.appendChild(s);
-        }
-    } 
-    catch (e){
-        console.log(e);
-        return false;
-    }
-    return true;
-}
-
-/**
-    * Pulls scripts from given html
-    * 
-    * @param {string} html The html that you want to execute the scripts from
-    * @returns {[[string, boolean]]} Index 1 contains scripts/src values; 
-    * index 2 states whether it's a src (true) or an embedded script (false)
-*/
-function ExtractScripts(html){
-    const scripts = [[,]]; // Index 1 contains scripts/src values; 
-                             // index 2 states whether it's a src (true) or an embedded script (false)
-
-    // js file extensions
-    const jsExts = ["js", "ts", "es", "es6", "ls", "sjs", "eg", "json",
-                         "json5", "cs", "coffee", "coffee.md", "mjs"]
-
-    // Initial match for script src
-    let srcRegEx = "";
-    // To test when dividing scripts by src or embedded
-    let testSrcRegEx = "";
-    // Removes the "<script" fluff before the src path/url
-    let rmSrcBeginning = /(?!<script.*src=)["'].*/ui;
-
-    // Initial match for embedded script
-    const embedRegEx = /<script>[\s\S]*?<\/script>/gui;
-    // Only grabs actual script content (plus wrapping carats)
-    const embedScriptContent = />+?[\s\S]*</ui;
-
-    // Creates srcRegEx
-    for(i = 0; i < jsExts.length; i++){
-        srcRegEx += `<script.*src=["'].*.${jsExts[i]}["']?`;
-        if(i != jsExts.length - 1){
-            srcRegEx += "|";
-        }
-    }
-
-    srcRegEx = new RegExp(srcRegEx, "gui");
-
-    // Creates testSrcRegEx
-    for(i = 0; i < jsExts.length; i++){
-        testSrcRegEx += `^<script.*src=["'].*.${jsExts[i]}["'](?!.*<)`;
-        if(i != jsExts.length - 1){
-            testSrcRegEx += "|";
-        }
-    }
-    testSrcRegEx = new RegExp(testSrcRegEx, "ui");
-
-    const combinedRegEx = new RegExp("(" + srcRegEx.source + ")|(" + embedRegEx.source + ")", "gui");
-    const rawScripts = html.toString().match(combinedRegEx);
-
-    for(i in rawScripts){
-        let script = rawScripts[i];
-        const testScript = script.match(testSrcRegEx);
-
-        let isSrc;
-
-        if(testScript){ // If a script src
-            script = testScript.toString().match(rmSrcBeginning).toString();
-            isSrc = true;
-        }
-        else{
-            script = script.match(embedScriptContent).toString();
-            script = script.replace(/\s+/g, " ");
-            isSrc = false;
-        }
-        script = script.trim().substring(1, script.length - 1).toString();
-        scripts.push([script, isSrc]);
-    }
     
-    return(scripts);
+            SetFields(){
+                // js file extensions
+                this.jsExts = ["js", "ts", "es", "es6", "ls", "sjs", "eg", "json",
+                "json5", "cs", "coffee", "coffee.md", "mjs"]
+    
+                // Initial match for script src
+                this.srcRegEx = "";
+                // To test when dividing scripts by src or embedded
+                this.testSrcRegEx = "";
+                // Removes the "<script" fluff before the src path/url
+                this.rmSrcBeginning = /(?!<script.*src=)["'].*/ui;
+    
+                // Initial match for embedded script
+                this.embedRegEx = /<script>[\s\S]*?<\/script>/gui;
+                // Only grabs actual script content (plus wrapping carats)
+                this.embedScriptContent = />+?[\s\S]*</ui;
+                // Declares for later use
+                this.combinedRegEx;
+            }
+    
+            CreateSrcRegEx(){ // Creates srcRegEx
+                // Creates the initial regex to test for script with a src
+                // by using the extensions from "jsExts"
+                for(let i = 0; i < this.jsExts.length; i++){
+                    this.srcRegEx += `<script.*src=["'].*.${this.jsExts[i]}["']?`;
+                    if(i != this.jsExts.length - 1){ // insert an "|" alternation if not at the end
+                        this.srcRegEx += "|";
+                    }
+                }
+    
+                this.srcRegEx = new RegExp(this.srcRegEx, "gui");
+    
+                // Creates testSrcRegEx
+                // Creates regex used to distinguish between src and embedded scripts
+                for(let i = 0; i < this.jsExts.length; i++){
+                    this.testSrcRegEx += `^<script.*src=["'].*.${this.jsExts[i]}["'](?!.*<)`;
+                    if(i != this.jsExts.length - 1){ // insert an "|" alternation if not at the end
+                        this.testSrcRegEx += "|";
+                    }
+                }
+                this.testSrcRegEx = new RegExp(this.testSrcRegEx, "ui");
+                
+                // Combines srcRegEx and embedRegEx to initally search/grab all script tags from html
+                // Does not currently support minified html
+                // Also doesn't work in some edge cases that have urls with
+                // given extensions (i.e. ".js") and a " or ' within the src url
+                this.combinedRegEx = new RegExp("(" + this.srcRegEx.source + ")|(" + this.embedRegEx.source + ")", "gui");
+            }
+        }
+        this.LSA_REGEX = new LSA_REGEX_Class();
+    }
+
+    /**
+        * Replaces html on page with given html, executing scripts in the process.
+        * @param {string} html Html to replace current html
+        * @param {HTMLElement} domLocation Node whose html should be replaced
+        * 
+        * @returns {boolean} True if success; false if failure
+    */
+    ReplaceHtml(html, domLocation){
+        try{
+            const dp = new DOMParser();
+            // parses html from DOM
+            const doc = dp.parseFromString(html.toString(), "text/html");
+
+            // Clears current element given by "domLocation"
+            // Clones the dom location
+            const domLocationCopy = domLocation.cloneNode(false);
+            // Clears the given section of the dom
+            domLocation.parentNode.replaceChild(domLocationCopy, domLocation);
+
+            this.PlaceElems(doc.head.childNodes, domLocationCopy, true);
+            this.PlaceElems(doc.body.childNodes, domLocationCopy);
+        }
+        catch(e){
+            console.log(e);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if there are any script tags in the node
+     * 
+     * @param {Node} node Node to check for scripts
+     * 
+     * @returns {boolean} True if it contains scripts; false if it doesn't
+     */
+    CheckForScripts(node){
+        return (node.innerHTML.match(this.LSA_REGEX.combinedRegEx) ? true : false);
+    }
+
+    /**
+     * Recursively places html elements on the DOM
+     * 
+     * @param {HTMLCollection} elems Elements to place on DOM
+     * @param {Node} domLocation Where to put the elements
+     * 
+     * @returns {boolean} True if success; false if failure
+     */
+    PlaceElems(elems, domLocation, isHead = false, checkScripts = true){
+        // Checks if elems contain scripts. If not, just user innerHTML
+        // Inspired by jQuery
+        if(checkScripts){
+            if(!this.CheckForScripts(elems[0].parentNode)){
+                domLocation.innerHTML = elems;
+                return true;
+            }
+        }
+        // Iterates through elements in array
+        for(let elem of elems){       
+            if(elem.tagName === "SCRIPT"){ // If element is a script
+                const script = document.createElement("script"); // Creates a new script element
+                const src = elem.src; // Grabs src, if it exists
+                if(src && src !== ""){ // If src script
+                    script.src = src; // Sets src
+                }
+                else{ // If embedded script
+                    const code = document.createTextNode(elem.innerHTML); // Creates text node
+                    script.appendChild(code); // Puts embedded code inside of it
+                }
+                domLocation.appendChild(script); // Adds script to document
+            }
+            else{ // If not a script
+                try{
+                    // If elem contains child elements
+                    if(elem.children !== undefined){ // If elem has the "children" property
+                        if (elem.children.length > 0){ // If elem has at least 1 child
+                            const elemCopy = elem.cloneNode(false); // Clone the element
+                            domLocation.appendChild(elemCopy); // Append the childless element to the DOM
+                            
+                            // Recurse with the elems child nodes and 
+                            this.PlaceElems(elem.childNodes, elemCopy, isHead,  false);
+                        }
+                        else{ // If 0 child elements
+                            domLocation.appendChild(elem); // Append the element to the DOM
+                        }
+                    }
+                }
+                catch(e){
+                    console.log(elem);
+                    console.log(e);
+                }
+            }
+        }
+    }
+
 }
-
-// This was a test
-// v  v  v  v  v  v  v
-// fetch("/products/", {
-//     headers: {
-//         "X-Requested-With": "XMLHttpRequest"
-//     }
-// })
-// .then((response)=>{
-//     return response.text();
-// })
-// .then(html=>{
-//     let dp = new DOMParser();
-//     let h = dp.parseFromString(html, "text/html");
-
-//     ReplaceHtml(html, document.getElementsByClassName("main-cont")[0]);
-// });
